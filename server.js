b6c0c5b250abf29331cf8866b22362650dcfc9ed
laila890 <your-new-public-email@example.com>
2026-07-22 14:13:37 +0530

    add server.js

--- server.js
+++ server.js
@@ -1929,6 +1929,7 @@ app.post("/auth/2fa/setup", authenticateToken, async (req, res) => {
   try {
     const secret = speakeasy.generateSecret({
       name: `VigozenCRM (${req.user.email})`,
+      issuer: "VigozenCRM",
     });
 
     await pool.query(
